fa8f932e240609ab4250da37ce4240a76ca829b9
resserops <resserops@gmail.com>
2025-10-16 11:12:14 +0800

    refactor ps1.sh to enhance code readability

--- ps1/ps1.sh
+++ ps1/ps1.sh
@@ -1,5 +1,10 @@
 #! /usr/bin/env bash
 
+COLOR_YELLOW_BOLD='\e[01;33m'
+COLOR_GREEN_BOLD='\e[01;32m'
+COLOR_RESET='\e[0m'
+
+# 获取git当前分支修改状态，暂存区有修改显示'*'，工作区有新增文件显示'+'
 function git_dirty() {
     git_status=$(git status 2> /dev/null | tail -n1)
     if [[ ${git_status:0:17} == "nothing to commit" ]]; then
@@ -11,8 +16,14 @@ function git_dirty() {
     fi
 }
 
+# 获取git当前分支名称
 function git_branch() {
     git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e "s/* \(.*\)/[\1$(git_dirty)]/"
 }
 
-export PS1='[\u@$(hostname) \[\e[01;33m\]\w\[\e[0m\]]\[\e[01;32m\]$(git_branch)\[\e[0m\]\$ '
+# $1：颜色编码；$2：字符串，该函数返回未转义的完整PS1字符串
+function color() {
+    printf '\[%s\]%s\[%s\]' "$1" "$2" ${COLOR_RESET}
+}
+
+export PS1="[\u@$(hostname) $(color ${COLOR_YELLOW_BOLD} '\w')]$(color ${COLOR_GREEN_BOLD} '$(git_branch)')\$ "
